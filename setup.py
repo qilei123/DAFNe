a60125345e497937402cd9ef20a8380121290c9a
qilei123 <qileimail123@gmail.com>
2021-09-30 01:00:15 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup  
+
+setup(
+        name='dafne',     # 包名字
+        version='1.0',   # 包版本
+        description='This is a test of the setup for dafne',   # 简单描述
+        author='qilei',  # 作者
+        author_email='qileimail123@gmail.com',  # 作者邮箱
+        url='https://github.com/qilei123/DAFNe',      # 包的主页
+        packages=['dafne'],                 # 包
+)
