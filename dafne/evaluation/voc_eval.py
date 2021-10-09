d20ad1922396e74299626112772af1d4c681098b
qilei123 <qileimail123@gmail.com>
2021-10-09 01:26:04 -0400

    test

--- dafne/evaluation/voc_eval.py
+++ dafne/evaluation/voc_eval.py
@@ -105,21 +105,22 @@ def voc_eval(
 
     splitlines = [x.strip().split(" ") for x in lines]
 
-    if len(splitlines[0])>10:
-        temp_splitlines = []
-        for splitline in splitlines:
-            temp_splitline = []
-            out_len = len(splitline)-10
-            temp_filename = ''
-            for i in range(out_len):
-                temp_filename+=splitline[i]
-                temp_filename+=' '
-            temp_filename+=splitline[out_len]
-            temp_splitline.append(temp_filename)
-            for i in range(out_len+1,len(splitlines[0])):
-                temp_splitline.append(splitline[i])
-            temp_splitlines.append(temp_splitline)
-        splitlines = temp_splitlines    
+    
+    temp_splitlines = []
+    for splitline in splitlines:
+        temp_splitline = []
+        out_len = len(splitline)-10
+        temp_filename = ''
+        for i in range(out_len):
+            temp_filename+=splitline[i]
+            temp_filename+=' '
+        temp_filename+=splitline[out_len]
+        temp_splitline.append(temp_filename)
+        for i in range(out_len+1,len(splitlines[0])):
+            temp_splitline.append(splitline[i])
+        temp_splitlines.append(temp_splitline)
+    
+    splitlines = temp_splitlines    
 
 
     image_ids = [x[0] for x in splitlines]
