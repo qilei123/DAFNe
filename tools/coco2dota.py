a141588e0484d41cd8515af3ac94b9f0a372b0b9
qilei123 <qileimail123@gmail.com>
2021-10-09 01:06:30 -0400

    test

--- tools/coco2dota.py
+++ tools/coco2dota.py
@@ -7,16 +7,26 @@ coco = COCO(annFile)
 
 imgIds = coco.getImgIds()
 
+catIds = coco.getCatIds()
+
+coco.loadCats()
+
 save_folder_dir = "/home/qilei/DATASETS/trans_drone/"
 
 for imgId in imgIds:
-    print(imgId)
     img = coco.loadImgs(imgId)[0]
-    print(img['file_name'])
     save_dir = os.path.join(save_folder_dir,"labelTxt",img['file_name'].replace("jpg","txt"))
     file_holder = open(save_dir,'w')
     annIds = coco.getAnnIds(imgId)
     anns = coco.loadAnns(annIds)
 
     for ann in anns:
-        print(ann)
+        if len(ann['segmentation'][0])==8:
+            line = ''
+            for x_y in ann['segmentation'][0]:
+                line+=str(x_y)
+                line+=' '
+            line+=coco.loadCats(ann['category_id'])
+            line+=' '
+            line+='0\n'
+            file_holder.write(line)
