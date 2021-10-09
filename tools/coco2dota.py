26e10fc798ce65ac9a57ed00fdd812cce4fa48ec
qilei123 <qileimail123@gmail.com>
2021-10-09 00:54:09 -0400

    test

---
+++ tools/coco2dota.py
@@ -0,0 +1,20 @@
+from pycocotools.coco import COCO
+import json
+import os
+
+annFile = "/home/qilei/DATASETS/trans_drone/annotations/test_mix.json"
+coco = COCO(annFile)
+
+imgIds = coco.getImgIds()
+
+save_folder_dir = "/home/qilei/DATASETS/trans_drone/annotations"
+
+for imgId in imgIds:
+    img = coco.loadImgs(imgId)
+    save_dir = os.path.join(save_folder_dir,"labelTxt",img['file_name'].replace("jpg","txt"))
+    file_holder = open(save_dir,'w')
+    annIds = coco.getAnnIds(imgId)
+    anns = coco.loadAnns(annIds)
+
+    for ann in anns:
+        print(ann)
