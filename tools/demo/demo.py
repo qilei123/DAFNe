097f7a39cfa05677e0e723e03a6c2ea49f3f5735
qilei123 <qileimail123@gmail.com>
2021-10-12 16:28:59 -0400

    test

--- tools/demo/demo.py
+++ tools/demo/demo.py
@@ -187,7 +187,18 @@ class VisualizationDemo(object):
             vis_output (VisImage): the visualized image output.
         """
         vis_output = None
-        predictions = self.predictor(image)
+        for i in range(20):
+            start_time = time.time()
+            predictions = self.predictor(image)
+            logger.info(
+                "{}: {} in {:.2f}s".format(
+                    path,
+                    "detected {} instances".format(len(predictions["instances"]))
+                    if "instances" in predictions
+                    else "finished",
+                    time.time() - start_time,
+                )
+            )            
         # Convert image from OpenCV BGR format to Matplotlib RGB format.
         image = image[:, :, ::-1]
         visualizer = Visualizer(image, self.metadata, instance_mode=self.instance_mode)
@@ -229,18 +240,18 @@ if __name__ == "__main__":
     for path in tqdm.tqdm(args.input, disable=not args.output):
         # use PIL, to be consistent with evaluation
         img = read_image(path, format="BGR")
-        for i in range(20):
-            start_time = time.time()
-            predictions, visualized_output = demo.run_on_image(img)
-            logger.info(
-                "{}: {} in {:.2f}s".format(
-                    path,
-                    "detected {} instances".format(len(predictions["instances"]))
-                    if "instances" in predictions
-                    else "finished",
-                    time.time() - start_time,
-                )
+        
+        start_time = time.time()
+        predictions, visualized_output = demo.run_on_image(img)
+        logger.info(
+            "{}: {} in {:.2f}s".format(
+                path,
+                "detected {} instances".format(len(predictions["instances"]))
+                if "instances" in predictions
+                else "finished",
+                time.time() - start_time,
             )
+        )
 
         if os.path.isdir(args.output):
             assert os.path.isdir(args.output), args.output
